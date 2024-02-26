b2384a5a237ec3bf3e3711666eff185d6ebe68d6
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2024-02-26 17:43:20 -0500

    3. Category Controller, Index View (1/16)

---
+++ Controllers/MakeController.cs
@@ -0,0 +1,22 @@
+﻿using CarsSpring2024.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsSpring2024.Controllers
+{
+    public class MakeController : Controller
+    {
+        private CarsDbContext _dbContext;
+
+        public MakeController(CarsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()//list or fetch all objects (categories)
+        {
+            var listOfMakes = _dbContext.Makes.ToList();
+
+            return View(listOfMakes);
+        }
+    }
+}
